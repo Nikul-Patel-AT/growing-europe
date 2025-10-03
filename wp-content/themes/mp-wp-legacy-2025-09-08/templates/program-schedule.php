a735f98e6f826791fc2f378b41baec3baea1e4e2
Kartik-Patel-AT <kartik.patel@agreemtech.com>
2025-10-03 15:44:24 +0530

    add-target-balnak

--- wp-content/themes/mp-wp-legacy-2025-09-08/templates/program-schedule.php
+++ wp-content/themes/mp-wp-legacy-2025-09-08/templates/program-schedule.php
@@ -1,11 +1,11 @@
 <section class="program-schedule padding-top-56">
     <div class="container">
         <div class="program-schedule__flex">
-            <?php if( have_rows('program_schedule_box') ): ?>
-                <?php while( have_rows('program_schedule_box') ): the_row(); ?>
+            <?php if (have_rows('program_schedule_box')): ?>
+                <?php while (have_rows('program_schedule_box')): the_row(); ?>
                     <?php $design = get_sub_field('select_design'); ?>
 
-                    <?php if( $design == 'design_one' ): ?>
+                    <?php if ($design == 'design_one'): ?>
                         <div class="program-item hideme">
                             <div class="program-item__number">
                                 <?php the_sub_field('number'); ?>
@@ -13,7 +13,7 @@
                             <h3 class="program-item__heading font--h4-500 darkblue_color">
                                 <?php echo wp_kses_post(get_sub_field('heading')); ?>
                             </h3>
-                            <?php if ( get_sub_field('time') ) : ?>
+                            <?php if (get_sub_field('time')) : ?>
                                 <div class="program-item__time font--h4-500 darkblue_color">
                                     <?php the_sub_field('time'); ?>
                                 </div>
@@ -23,20 +23,31 @@
                             </div>
                         </div>
 
-                    <?php elseif( $design == 'design_two' ): ?>
-                        <div class="program-item program-item--highlight hideme">
-                            <div class="program-item__number font--h4-500">
-                                <?php the_sub_field('part_number'); ?>
-                            </div>
-                            <div class="program-item__content-hightlighted">
-                                <h3 class="program-item__title font--h3-700">
-                                    <?php the_sub_field('title'); ?>
-                                </h3>
-                                <div class="program-item__desc font--p-16">
-                                    <?php the_sub_field('description'); ?>
+                        <?php elseif ($design == 'design_two'):
+                        $program_link = get_sub_field('program_link'); // ACF URL field
+                        $program_target = get_sub_field('program_target'); // ACF target field (_blank, _self, etc.)
+
+                        if ($program_link) { ?>
+                            <a href="<?php echo esc_url($program_link); ?>" class="program-item-link"  target="<?php echo esc_attr($program_target); ?>">
+                        <?php } ?>
+                            <div class="program-item program-item--highlight hideme">
+                                <div class="program-item__number font--h4-500">
+                                    <?php the_sub_field('part_number'); ?>
+                                </div>
+                                <div class="program-item__content-hightlighted">
+                                    <h3 class="program-item__title font--h3-700">
+                                        <?php the_sub_field('title'); ?>
+                                    </h3>
+                                    <div class="program-item__desc font--p-16">
+                                        <?php the_sub_field('description'); ?>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        <?php if ($program_link){ ?>
+                        </a>
+                        <?php } ?>
+
+
                     <?php endif; ?>
 
                 <?php endwhile; ?>
