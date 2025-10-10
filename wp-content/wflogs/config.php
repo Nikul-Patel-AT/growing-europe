9f324b539e77468dfe79b6f8810e200bcb3920d9
akash.patel <akash.patel@civitta.com>
2025-10-10 15:02:22 +0530

    latest-code

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"R@F CqPH/Su/h,8zr8UwE.fG])|m>nPLExKZWe%4#`.5O_Ki1pe_%lUzSN7i;7J,";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:1;s:13:"attackDataKey";i:3487;}
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"R@F CqPH/Su/h,8zr8UwE.fG])|m>nPLExKZWe%4#`.5O_Ki1pe_%lUzSN7i;7J,";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3487;}
